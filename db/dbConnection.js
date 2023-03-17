a3603a2868f358f5368cca72758d648d30dbd045
Matheus-Loubach <matheus.loubach@gmail.com>
2023-03-17 13:26:20 -0300

    up

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -6,7 +6,7 @@ async function main(){
     try {
         process.env.REACT_APP_FLY_API
         mongoose.set("strictQuery", true)
-        await mongoose.connect('mongodb+srv://MatheusLoubach:CQrv8ZchRqgyqVx1@cluster0.mnqn7sq.mongodb.net/?retryWrites=true&w=majority');
+        await mongoose.connect('mongodb+srv://matheusLoubach:Bz1nKg06fEek5ihc@cluster0.mnqn7sq.mongodb.net/?retryWrites=true&w=majority');
 
         console.log("Banco Conectado", mongoose.connection.host);
     } catch (error) {
